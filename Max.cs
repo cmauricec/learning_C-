d042b063055255fe2b6f25aff56a9452dd836705
cmauricec <99288186+cmauricec@users.noreply.github.com>
2023-02-14 22:34:49 -0500

    Add files via upload

---
+++ Max.cs
@@ -0,0 +1,31 @@
+﻿//Christopher Maurice 
+//Lab 3 
+//Task 5 
+namespace MyMaxLib
+{
+    public class Max
+    {
+        public static double Mymax(double a, double b, double c, double number)
+        {
+            double max;
+            if (number == 3)
+            {
+                double[] arr = { a, b, c };
+                max = arr.Max();
+                Console.WriteLine("The max of " + a + ", " + b + ", " 
+                + c + " is " + max);
+            }
+            else
+            {
+                max = Math.Max(a, b);
+                Console.WriteLine("The max of " + a + ", " + b + "is" + max);
+            }
+            return max;
+
+        }
+
+    }
+
+}
+
+
